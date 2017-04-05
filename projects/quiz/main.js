b0f372c47ad0c217bb0bf22e52568f18091d08c2
Elizabeth Tsai <tsaie299@newschool.edu>
2017-04-05 19:47:00 -0400

    upload javascript quiz online

---
+++ projects/quiz/main.js
@@ -0,0 +1,36 @@
+var name = "Liz";
+var seven = 6+1;
+var numstring = "seven";
+var x = 0;
+  console.log(1 > x);
+var letters = [l, i, z];
+var data = [1, z, "data"];
+var last = "Tsai";
+  console.log(name + last);
+50 + 50;
+
+var firstlast = name[name.length-3, name.length - 1];
+var word = ‘supercalifragilisticexpialidocious’;
+  console.log(word.length);
+
+  console.log(name.length = last.length);
+
+var students;
+if (students = 10) {
+  console.log("all the students are in class");
+}
+else {
+  console.log("not everyone is here");
+}
+
+
+for (var letters = 0; letters < letters.length; letters++) {
+  console.log(letter);
+
+var time;
+if (time > 7 && time < 9:40); {
+  console.log("yes");
+}
+else {
+  console.log("no");
+}
