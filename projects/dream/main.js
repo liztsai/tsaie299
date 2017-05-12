87753b72ff4e462091e802c2b8fc86a959dfd448
Elizabeth Tsai <tsaie299@newschool.edu>
2017-05-11 22:48:26 -0400

    add dream

--- projects/dream/main.js
+++ projects/dream/main.js
@@ -4,67 +4,70 @@ function rain() {
     if ( audio.paused ) {
       audio.play();
     } else {
-      audio.pause()
+      audio.pause();
     }
 }
 
 function thunder() {
     var audio2 = document.getElementById("audio2");
 
-    if ( audio.paused ) {
-      audio.play();
+    if ( audio2.paused ) {
+      audio2.play();
     } else {
-      audio.pause()
+      audio2.pause();
     }
 }
 
 function bird() {
     var audio3 = document.getElementById("audio3");
 
-    if ( audio.paused ) {
-      audio.play();
+    if ( audio3.paused ) {
+      audio3.play();
     } else {
-      audio.pause()
+      audio3.pause();
     }
 }
 
 function city() {
     var audio4 = document.getElementById("audio4");
 
-    if ( audio.paused ) {
-      audio.play();
+    if ( audio4.paused ) {
+      audio4.play();
     } else {
-      audio.pause()
+      audio4.pause();
     }
 }
 
 function fire() {
     var audio5 = document.getElementById("audio5");
 
-    if ( audio.paused ) {
-      audio.play();
+    if ( audio5.paused ) {
+      audio5.play();
     } else {
-      audio.pause()
+      audio5.pause();
     }
 }
 
 function honk() {
     var audio6 = document.getElementById("audio6");
-
-    if ( audio.paused ) {
-      audio.play();
+    $('#flash').click(function hoverFunction() {
+        $(this).fadeOut(10);
+        $(this).fadeIn(500);
+    });
+    if (audio6.paused) {
+        audio6.play();
     } else {
-      audio.pause()
+        audio6.pause();
     }
 }
 
 function wind() {
     var audio7 = document.getElementById("audio7");
 
-    if ( audio.paused ) {
-      audio.play();
+    if ( audio7.paused ) {
+      audio7.play();
     } else {
-      audio.pause()
+      audio7.pause();
     }
 }
 
@@ -73,35 +76,32 @@ function rain1() {
     rain1.toggle();
 }
 
+function thunder2() {
+    var thunder2 = $('.thunder2.background');
+    thunder2.toggle();
+}
+
+function bird2() {
+    var bird2 = $('.bird2.background');
+    bird2.toggle();
+}
+
 function city2() {
     var city2 = $('.city2.background');
     city2.toggle();
 }
 
 function fire2() {
-    var fire2 = document.getElementById('fire2');
-    if (fire2.style.display === 'none') {
-        fire2.style.display = 'block';
-    } else {
-        fire2.style.display = 'none';
-    }
+    var fire2 = $('.fire2.background');
+    fire2.toggle();
 }
 
 function honk2() {
-    var honk2 = document.getElementById('honk2');
-    if (honk2.style.display === 'none') {
-        honk2.style.display = 'block';
-    } else {
-        honk2.style.display = 'none';
-    }
+    var honk2 = $('.honk2.background');
+    honk2.toggle();
 }
 
-
 function wind2() {
-    var wind2 = document.getElementById('wind2');
-    if (wind2.style.display === 'none') {
-        wind2.style.display = 'block';
-    } else {
-        wind2.style.display = 'none';
-    }
+    var wind2 = $('.wind2.background');
+    wind2.toggle();
 }
